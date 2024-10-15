02c5854789398b51cf04d74635b9d1d1807aec0c
Daniel Pedrinho <dpedrinho01@ua.pt>
2024-10-15 10:22:26 +0100

    Landing Page Creation

--- flutter_project_cm/lib/main.dart
+++ flutter_project_cm/lib/main.dart
@@ -9,11 +9,47 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      home: const LandingPage(),
+      routes: {
+        '/hello': (context) => const HelloWorldPage(),
+      },
+    );
+  }
+}
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('LandingPage'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/hello');
+              },
+              child: const Text('Enter'),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class HelloWorldPage extends StatelessWidget {
+  const HelloWorldPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello World!'),
       ),
     );
   }
